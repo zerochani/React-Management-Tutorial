b0c9b5cbf7dbb34c5bfd3860eaf2a45a36384156
zerochani <youngchan0510@gmail.com>
2024-09-13 15:39:08 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+const customers = [
+{
+  'id': 1,
+  'img': 'https//placeimg.com/64/64/1',
+  'name': '박영찬',
+  'birthday': '69453',
+  'gender': '남자',
+  'job': '대학생'
 
+},
+{
+  'id': 2,
+  'img': 'https//placeimg.com/64/64/2',
+  'name': '홍길동',
+  'birthday': '431414',
+  'gender': '남자',
+  'job': '프로그래머'
+
+},
+{
+  'id': 3,
+  'img': 'https//placeimg.com/64/64/2',
+  'name': '이순신',
+  'birthday': '124535',
+  'gender': '남자',
+  'job': '디자이너'
+
+}
+]
 class App extends Component {
   render(){
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
+      <div>
+        {
+          customers.map(c=> {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+
+            );
+          }
+
+          )
+        }
       </div>
-  
     );
   }
 }
